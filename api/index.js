7f1b27066bcf720e120be438b3d910bfd08d3499
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-06-14 18:02:05 +0500

    create test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 const app = express();
 mongoose
@@ -15,3 +16,9 @@ mongoose
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.use("/api/user", userRouter);
