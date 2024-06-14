09540f21aa7d95113e160ec4abdde90481f93f88
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-06-14 17:51:24 +0500

    create User Model

---
+++ api/models/User.model.js
@@ -0,0 +1,22 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.nodel("User", userSchema);
+export default User;
