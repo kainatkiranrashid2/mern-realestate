cf37106588365ba75cde1a425edd0dba51e80ab3
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-06-14 21:04:03 +0500

    complete sign in page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import AboutUs from "./pages/AboutUs";
 import Header from "./components/Header";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
   return (
@@ -13,7 +13,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
 
